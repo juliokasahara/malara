2a202e84416a0333374b34eefcc627f8a08216c6
Julio <juliokasahara@gmail.com>
2024-06-05 20:55:26 -0300

    teste

--- src/test/java/com/example/malara/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/malara/controller/EmployeeControllerTest.java
@@ -1,8 +1,16 @@
 package com.example.malara.controller;
 
 import com.example.malara.model.Employee;
+import com.example.malara.model.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 public class EmployeeControllerTest {
@@ -10,21 +18,25 @@ public class EmployeeControllerTest {
     @Mock
     private EmployeeController employeeController;
 
+    @MockBean
+    private EmployeeRepository repository;
+
     @Test
     public void testSoma() {
 
-//        when()
-//        employeeController.one(10L)
-//        assertEquals(5, resultado);
+//        when(repository.findById(any())).thenThrow(mockEmployee());
+//        var result = employeeController.one(10L);
+//        assertEquals(10L, result.getId());
     }
 
-    private Employee mockEmployee(){
+    private Optional<Employee> mockEmployee(){
         Employee employee = new Employee();
         employee.setId(10L);
         employee.setName("Tste");
         employee.setRole("ADM");
 
-        return employee;
+        Optional<Employee> optionalEmployee = Optional.of(employee);
+        return optionalEmployee;
     }
 
 }
